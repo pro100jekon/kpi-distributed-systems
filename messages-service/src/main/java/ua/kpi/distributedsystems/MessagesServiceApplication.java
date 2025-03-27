8c9d056c66570a38c16af62d9c6eaa11a470db38
Yevhenii Smyrnov <kalledat57@gmail.com>
2025-03-28 00:35:30 +0200

    create skeleton for the project

---
+++ messages-service/src/main/java/ua/kpi/distributedsystems/MessagesServiceApplication.java
@@ -0,0 +1,13 @@
+package ua.kpi.distributedsystems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagesServiceApplication.class, args);
+	}
+
+}
---
+++ messages-service/src/test/java/ua/kpi/distributedsystems/MessagesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kpi.distributedsystems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
